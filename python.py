57e0b6949a80a19728318814b5383898bc375a85
BrianTahiel <briang.2012@hotmail.com>
2023-04-21 00:01:05 -0300

    minor fixes

--- python.py
+++ python.py
@@ -1,16 +1,27 @@
-def sum(num1, num2):
-    suma = num1 + num2
-    return suma
-while True:
-    try: 
-        num1 = float(input("1° Número: "))
-        num2 = float(input("2° Número: "))
-        break
-    except ValueError:
-        print("Valor incorrecto")
-print(sum(num1, num2))
+# def sum(num1, num2):
+#     suma = num1 + num2
+#     return suma
+# while True:
+#     try: 
+#         num1 = float(input("1° Número: "))
+#         num2 = float(input("2° Número: "))
+#         break
+#     except ValueError:
+#         print("Valor incorrecto")
+# print(sum(num1, num2))
 
 
+
+
+import datetime
+
+fecha_actual = datetime.datetime.now()
+fecha = fecha_actual.strftime("%Y-%m-%d")
+hora = fecha_actual.strftime("%H:%M:%S")
+
+temp = float(input("Temperatura: "))
+mensaje = ("Temp: " + str(temp) + "°C " + fecha + hora)
+print(mensaje)
 
 
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-
 function sum() {
   let num1 = parseFloat(document.getElementById("num1").value);
   let num2 = parseFloat(document.getElementById("num2").value);
@@ -7,3 +6,16 @@ function sum() {
     document.getElementById("result").textContent = "La suma es: " + result;
   }
 }
+
+function temp(){
+  let fechaActual = new Date();
+  let fechaISO = fechaActual.toISOString(); 
+  let fecha = fechaISO.slice(0, 10);
+  let hora = fechaActual.toLocaleTimeString("es-AR");
+  let temp = parseFloat(document.getElementById('temp').value);
+  if(temp){
+    document.getElementById("resultado").textContent = "Temp: " + temp + "°C " + fecha + hora;
+  }
+}
+
+
