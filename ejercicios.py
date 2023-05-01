1ae989ab7f4e48add1f709084c0b5907275b9af4
BrianTahiel <briang.2012@hotmail.com>
2023-05-01 00:07:46 -0300

    minor changes

--- ejercicios.py
+++ ejercicios.py
@@ -1,10 +1,15 @@
 colores = ["Rojo", "Negro", "Blanco"]
 
 while True:
+    number = input("Ingrese un núm del 1 al 3 o salir: ")
+    if number.lower() == 'salir':
+        break
     try:
-        num = int(input("Ingrese un núm del 1 al 3: "))
+        num = int(number)
         if(num >= 1 and num <=3):
             eleccion = colores[num - 1]
-            print(eleccion)     
+            print(eleccion)
+        else:
+            print("Valor fuera del rango")     
     except ValueError:
         print("Ingrese un número válido")
